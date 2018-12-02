e72178fec2877a1295efc4744c4206acc59f664d
Diogo Nunes <diogo_p_nunes@hotmail.com>
2018-12-02 11:53:18 +0100

    added new rules

--- sketch.js
+++ sketch.js
@@ -1,8 +1,9 @@
 var seeds;
+var colors;
 var MAX_SEEDS;
 var rule = 1;
 var myPoint;
-var previousChoice;
+var previousChoice, secondPreviousChoice;
 
 function setup() {
   createCanvas(800, 600);
@@ -19,12 +20,16 @@ function updateMaxSeeds(value) {
   background(50);
   MAX_SEEDS = value;
   seeds = [];
+  colors = [];
+  previousChoice = null;
+  secondPreviousChoice = null;
   for(var i = 0; i < MAX_SEEDS; i++) {
     var angle = i * (TWO_PI / MAX_SEEDS);
     var v = p5.Vector.fromAngle(angle);
     v.mult(width / 3);
     v.add(width / 2, height / 2);
     seeds.push(v);
+    colors.push(color(random(255), random(255), random(255)));
   }
 }
 
@@ -51,14 +56,29 @@ function draw() {
         }
         previousChoice = choice;
         break;
+      case 3:
+        // cannot equal the previous choice clock-wise
+        if(choice == previousChoice) {
+          continue;
+        }
+        previousChoice = (choice + 1) % MAX_SEEDS;
+        break;
+      case 4:
+        // cannot equal the previous choice clock-wise
+        if(choice == previousChoice) {
+          continue;
+        }
+        previousChoice = (choice + 2) % MAX_SEEDS;
+        break;
     }
       
     myPoint.x = lerp(myPoint.x, seeds[choice].x, 0.5);
     myPoint.y = lerp(myPoint.y, seeds[choice].y, 0.5);
+    
 
     // draw
     strokeWeight(1);
-    stroke(255, 0, 0);
+    stroke(colors[choice]);
     point(myPoint.x, myPoint.y);
   }
 }
